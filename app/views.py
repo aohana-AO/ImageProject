75c0aca414e9e3739e37a8d6b29e6002e4d2265b
aoiyamada <aoihana1130@gmail.com>
2023-11-06 11:29:30 +0900

    バックpost追加

--- app/views.py
+++ app/views.py
@@ -6,5 +6,14 @@ from django.views.generic import View
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        list = ['a','b','c','d','e']
-        return render(request, 'app/index.html', {'list':list})
+        return render(request, 'app/index.html')
+
+    def post(self, request, *args, **kwargs):
+        image_name = request.POST.get('image_name')
+
+        print(image_name)
+
+        # ここで画像の処理を実装する。例えば、画像名に基づいて画像を取得してHttpResponseを返す。
+        # response = ...
+
+        return render(request, 'app/index.html')
