abc3dbd7b559a4ae0f22464202c3b6dc015472a2
kbc21a02 <kbc21a02@stu.kawahara.ac.jp>
2023-12-04 09:39:06 +0900

    変更内容のメッセージ

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ import time
 import environ
 
 def image_create(prompt, image_name):
-    engine_id = "stable-diffusion-xl-1024-v1-0"
+    engine_id = "stable-diffusion-xl-beta-v2-2-2"
     api_host = os.getenv('API_HOST', 'https://api.stability.ai')
     env = environ.Env()
     env.read_env('.env')
@@ -72,48 +72,35 @@ class IndexView(View):
         return render(request, 'app/index.html',{'image_name': image_name})
 
     def image_create(prompt,image_name):
-        engine_id = "stable-diffusion-xl-1024-v1-0"
+        engine_id = "stable-diffusion-xl-beta-v2-2-2"
         api_host = os.getenv('API_HOST', 'https://api.stability.ai')
-        api_keys = ["sk-tYk6w9Fme97inuhtUIHXkbNlsrjQoI5GKjJWaN2qCHwmrWy3","sk-MfErYuFrlHoCCAq38rjMXkI81pnsTsx0IgtCqpE23JpbUuo9","sk-qAkGDzTUQs8qYWabfoSJ7ZmlSSv4uXz28JSrA2lJtckXwFij","sk-40whWWW9vOenzVjPxPN0cSVk5HCqKXCadmiRHkEcvyJ1KmJu"]
-        current_api_key_index = 0  # 現在のAPIキーのインデックス
-        Style_preset = ["3d-model", "analog-film","anime","cinematic","comic-book","digital-art","enhance","fantasy-art","isometric","line-art","low-poly","modeling-compound","neon-punk","origami","photographic","pixel-art","tile-texture"]
-        while current_api_key_index < len(api_keys):
-            # API Keyの取得確認
-            if api_keys[current_api_key_index] is None:
-                raise Exception("Missing Stability API key.")
-            current_api_key = api_keys[current_api_key_index]
-            # API呼び出し
-            response = requests.post(
-                f"{api_host}/v1/generation/{engine_id}/text-to-image",
-                headers={
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                    "Authorization": f"Bearer {current_api_key}"
-                },
-                json={
-                    #"Samples": 1,画像生成枚数の指定
-                    #"style_preset": Style_preset[6],
-                    "text_prompts": [
-                        {
-                            "text": prompt
-                        },
-                    #{#ネガティブプロンプト
-                    #"text": "",
-                    #"weight": -1.0
-                    #},
-                    ],
-                },
-            )
+        api_key = "sk-tYk6w9Fme97inuhtUIHXkbNlsrjQoI5GKjJWaN2qCHwmrWy3"
+
+        # API Keyの取得確認
+        if api_key is None:
+            raise Exception("Missing Stability API key.")
+
+        # API呼び出し
+        response = requests.post(
+            f"{api_host}/v1/generation/{engine_id}/text-to-image",
+            headers={
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": f"Bearer {api_key}"
+            },
+            json={
+                "text_prompts": [
+                    {
+                        "text": prompt
+                    }
+                ],
+            },
+        )
+
         # レスポンス確認
-            if response.status_code == 200:
-                break  # APIキーが有効な場合はループを抜けます
-            else:
-        # APIキーが無効な場合、次のAPIキーに切り替えます
-                current_api_key_index += 1
-        # レスポンス確認
-            if response.status_code != 200:
-                raise Exception("Non-200 response: " + str(response.text))
-            
+        if response.status_code != 200:
+            raise Exception("Non-200 response: " + str(response.text))
+
         # レスポンス取得
         data = response.json()
 
