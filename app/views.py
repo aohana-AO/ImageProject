b174d342356ee90c8a1c5ffb3b7e86d5a93bdf83
kbc21a02 <kbc21a02@stu.kawahara.ac.jp>
2023-12-05 09:46:30 +0900

    スタイル指定機能実装

--- app/views.py
+++ app/views.py
@@ -11,16 +11,19 @@ import environ
 from .models import Image
 
 
-def image_create(prompt, negative_prompt, image_name):
+def image_create(prompt, negative_prompt, image_name,number):
     engine_id = "stable-diffusion-xl-1024-v1-0"
     api_host = os.getenv('API_HOST', 'https://api.stability.ai')
     env = environ.Env()
     api_keys = env('IMAGE_API_KEY').split(',')
 
+    number = int(number)
     current_api_key_index = 0  # 現在のAPIキーのインデックス
+
     Style_preset = ["3d-model", "analog-film", "anime", "cinematic", "comic-book", "digital-art", "enhance",
                     "fantasy-art", "isometric", "line-art", "low-poly", "modeling-compound", "neon-punk", "origami",
                     "photographic", "pixel-art", "tile-texture"]
+    style = Style_preset[number]
     while current_api_key_index < len(api_keys):
         # API Keyの取得確認
         if api_keys[current_api_key_index] is None:
@@ -36,7 +39,7 @@ def image_create(prompt, negative_prompt, image_name):
             },
             json={
                 # "Samples": 1,画像生成枚数の指定
-                # "style_preset": Style_preset[6],
+                "style_preset": style,
                 "text_prompts": [
                     {
                         "text": prompt
@@ -80,20 +83,23 @@ class IndexView(View):
         prompt = request.POST.get('prompt')
         negative_prompt = request.POST.get('negative_prompt')
         image_name = f"img/{prompt}_{negative_prompt}.png"
-
+        number = request.POST.get('number')
+        print(number)
         print(image_name)
-        image_create(prompt, negative_prompt, f'static/{image_name}')
+        # 'number'パラメータをimage_create関数に渡す
+        image_create(prompt, negative_prompt, f'static/{image_name}', number)
 
         Image.objects.create(
             user_ID=self.request.user,
             image_path=image_name,
             prompt=prompt,
-            negative_prompt=negative_prompt
+            negative_prompt=negative_prompt,
+            ##selected_number=selected_number
         )
 
         Images = Image.objects.filter(user_ID=self.request.user)
 
-        return render(request, 'app/index.html', {'image_name': image_name, 'Images': Images})
+        return render(request, 'app/index.html', {'image_name': image_name, 'Images': Images, 'number': number})
 
 
 def image_detail_view(request, image_id):
